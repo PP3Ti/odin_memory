bda7e975c1ac2b115f2a0087fa684c9f58e22470
PP3Ti <porkolab.peti@gmail.com>
2023-06-28 17:11:50 +0200

    layout fix 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import title from '../assets/title.png'
 export default function Header() {
   return (
     <header> 
-      <img src={title} alt='pokemon title' height='150px'/>
+      <img src={title} alt='pokemon title' height='130px'/>
       <p>The Memory Game</p>
     </header>
   )
